5c3977406e5e44973aa395d431c31f51605d0d98
haris-584 <hariskhattak584@gmail.com>
2020-07-14 08:59:25 +0500

    initial commit

---
+++ HummingBird-Penguin-Haris/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MenuController : MonoBehaviour
+{
+    public Button penguinProject;
+    public Button hummingbirdProject;
+    public string penguinSceneName;
+    public string hummingbirdSceneName;
+
+    public void PenguinGame()
+    {
+        SceneManager.LoadScene(penguinSceneName);
+    }
+    public void HummingbirdGame()
+    {
+        SceneManager.LoadScene(hummingbirdSceneName);
+    }
+}
